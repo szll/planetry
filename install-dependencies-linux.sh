6a0b0e2f27324e08a52f15a8b073b6ae9475ede6
Steffen Zull <steffen.zull@gmail.com>
2018-01-06 15:58:25 +0100

    Next try

--- install-dependencies-linux.sh
+++ install-dependencies-linux.sh
@@ -3,5 +3,20 @@
 # install dep
 go get -u github.com/golang/dep/cmd/dep
 
-sudo apt-get install libsdl2-2.0-0
+# download and unzip sdl2
+curl -o sdl2.tar.gz http://libsdl.org/release/SDL2-2.0.7.tar.gz
+tar -xvzf sdl2.tar.gz
+
+# install sdl2
+cd SDL2-2.0.7
+mkdir build
+cd build
+../configure
+make
+sudo make install
+cd ../..
+
+# remove sdl2 dir
+rm -rf SDL2-2.0.7
+
 sudo apt-get install liblua5.1
