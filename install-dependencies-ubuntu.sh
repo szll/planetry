528023bf65eea6bd734a23a9c888fb4304bdba7e
Steffen Zull <steffen.zull@gmail.com>
2018-01-15 19:56:26 +0100

    Revert sdl2 building

--- install-dependencies-ubuntu.sh
+++ install-dependencies-ubuntu.sh
@@ -3,23 +3,21 @@
 # install dep
 go get -u github.com/golang/dep/cmd/dep
 
-# # download and unzip sdl2
-# curl -o sdl2.tar.gz http://libsdl.org/release/SDL2-2.0.7.tar.gz
-# tar -xvzf sdl2.tar.gz
+# download and unzip sdl2
+curl -o sdl2.tar.gz http://libsdl.org/release/SDL2-2.0.7.tar.gz
+tar -xvzf sdl2.tar.gz
 
-# # install sdl2
-# cd SDL2-2.0.7
-# mkdir build
-# cd build
-# ../configure
-# make
-# sudo make install
-# cd ../..
+# install sdl2
+cd SDL2-2.0.7
+mkdir build
+cd build
+../configure
+make
+sudo make install
+cd ../..
 
-# # remove sdl2 dir
-# rm -rf SDL2-2.0.7
-
-sudo apt install libsdl2-dev
+# remove sdl2 dir
+rm -rf SDL2-2.0.7
 
 # install lua
 sudo apt-get install liblua5.1
