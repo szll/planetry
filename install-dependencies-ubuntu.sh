b5cd54873f01630e6fa2e4dfc4d5d976233f698c
Steffen Zull <steffen.zull@gmail.com>
2019-11-17 16:43:53 +0100

    Logging

--- install-dependencies-ubuntu.sh
+++ install-dependencies-ubuntu.sh
@@ -2,14 +2,14 @@
 
 set +e
 
-# install dep
+echo "install dep"
 go get -u github.com/golang/dep/cmd/dep
 
-# download and unzip sdl2
+echo "download and unzip sdl2"
 curl -o sdl2.tar.gz http://libsdl.org/release/SDL2-2.0.9.tar.gz
 tar -xvzf sdl2.tar.gz 1> /dev/null
 
-# install sdl2
+echo "install sdl2"
 cd SDL2-2.0.9
 mkdir build
 cd build
@@ -18,8 +18,8 @@ make
 sudo make install
 cd ../..
 
-# remove sdl2 dir
+echo "remove sdl2 dir"
 rm -rf SDL2-2.0.9
 
-# install lua
+echo "install lua"
 sudo apt-get install liblua5.1
