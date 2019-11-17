acf571f2e763cd19cfd090792d0e40e4b0494c40
Steffen Zull <steffen.zull@gmail.com>
2019-11-17 16:23:16 +0100

    Redirect output of unzipping to dev null

--- install-dependencies-ubuntu.sh
+++ install-dependencies-ubuntu.sh
@@ -5,7 +5,7 @@ go get -u github.com/golang/dep/cmd/dep
 
 # download and unzip sdl2
 curl -o sdl2.tar.gz http://libsdl.org/release/SDL2-2.0.9.tar.gz
-tar -xvzf sdl2.tar.gz
+tar -xvzf sdl2.tar.gz 1> /dev/null
 
 # install sdl2
 cd SDL2-2.0.9
