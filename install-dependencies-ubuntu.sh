7456f141aa83385de24551d6904de5b6a01b4893
Steffen Zull <steffen.zull@gmail.com>
2019-11-17 16:58:47 +0100

    Update logging

--- install-dependencies-ubuntu.sh
+++ install-dependencies-ubuntu.sh
@@ -2,15 +2,15 @@
 
 set +e
 
-echo "install lua"
+echo "install liblua5.1"
 sudo apt-get update
 sudo apt-get install liblua5.1
 
-echo "download and unzip sdl2"
+echo "download and unzip sdl2 (2.0.9)"
 curl -o sdl2.tar.gz http://libsdl.org/release/SDL2-2.0.9.tar.gz
 tar -xvzf sdl2.tar.gz 1> /dev/null
 
-echo "install sdl2"
+echo "install sdl2 (2.0.9)"
 cd SDL2-2.0.9
 mkdir build
 cd build
